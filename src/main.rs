c7b8967fe4b56194e8bf821e454844c85957e24d
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-07-08 18:52:53 -0300

    project setup

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+const LOCAL_PLAYER: usize = 0x509B74;
+const HEALTH_OFFSET: usize = 0xF8;
+const RIFFLE_AMMO_OFFSET: usize = 0x150;
+const RIFLE_AMMO_RESERVE_OFFSET: usize = 0x128;
+const PISTOL_AMMO: usize = 0x13C;
+const NAME_OFFSET: usize = 0x225;
+const VEST_OFFSET: usize = 0xFC;
+
+
+fn main() {
+  println!("Hello, world!");
+  let game_window = loop {};
+}
